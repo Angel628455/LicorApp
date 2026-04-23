2721d1ca62181c1fa512e9b4f2359468028b0da4
Angel628455 <angel_morel@ucne.edu.do>
2026-04-23 01:09:52 -0400

    agregar OrdenarEntity.kt

---
+++ app/src/main/java/edu/ucne/licorapp/data/local/entity/OrdenarEntity.kt
@@ -0,0 +1,16 @@
+package edu.ucne.licorapp.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "pedidos")
+data class OrdenarEntity(
+    @PrimaryKey val id: Int,
+    val fecha: String,
+    val total: Double,
+    val estado: String?,
+    val direccionEnvio: String?,
+    val metodoPago: String?,
+    val usuarioId: Int,
+    val isSynced: Boolean = true
+)
