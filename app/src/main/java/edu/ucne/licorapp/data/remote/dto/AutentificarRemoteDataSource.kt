0b787a6810e58732c58e67155f10732ff0f6b097
Angel628455 <angel_morel@ucne.edu.do>
2026-04-23 00:25:08 -0400

    agregar AutentificarRemoteDataSource.kt

---
+++ app/src/main/java/edu/ucne/licorapp/data/remote/dto/AutentificarRemoteDataSource.kt
@@ -0,0 +1,16 @@
+package edu.ucne.licorapp.data.remote.dto
+
+import edu.ucne.licorapp.data.remote.dto.login.AutentificarResponse
+import edu.ucne.licorapp.data.remote.dto.login.LoginRequest
+import retrofit2.Response
+import javax.inject.Inject
+
+class AutentificarRemoteDataSource {
+//    suspend fun login(request: LoginRequest): Response<AutentificarResponse> {
+//        return api.login(request)
+//    }
+//
+//    suspend fun register(request: RegisterRequest): Response<Unit> {
+//        return api.register(request)
+//    }
+}
