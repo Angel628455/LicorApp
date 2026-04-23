e56d054419f2b7b7d4f039445bfd7419494ee9a5
Angel628455 <angel_morel@ucne.edu.do>
2026-04-23 01:23:37 -0400

    agregar UpdateMercanciaUseCase.kt

---
+++ app/src/main/java/edu/ucne/licorapp/domain/usecase/mercanciausecase/UpdateMercanciaUseCase.kt
@@ -0,0 +1,26 @@
+package edu.ucne.licorapp.domain.usecase.mercanciausecase
+
+import edu.ucne.licorapp.domain.model.Mercancia
+import edu.ucne.licorapp.domain.repository.MercanciaRepository
+import javax.inject.Inject
+
+class UpdateMercanciaUseCase @Inject constructor(
+    private val repository: MercanciaRepository
+) {
+    suspend operator fun invoke(mercancia: Mercancia): Result<Unit> {
+
+        if (mercancia.id <= 0) {
+            return Result.failure(Exception("Id inválido"))
+        }
+
+        if (mercancia.nombre.isBlank()) {
+            return Result.failure(Exception("El nombre no puede estar vacío"))
+        }
+
+        if (mercancia.precio < 0) {
+            return Result.failure(Exception("El precio no puede ser negativo"))
+        }
+
+        return repository.updateMercancia(mercancia)
+    }
+}
